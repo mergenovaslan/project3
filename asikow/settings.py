ba87f8c70c01e9cafca4f1c5f50777ab67435d3f
mergenovaslan <91003849+mergenovaslan@users.noreply.github.com>
2022-05-06 14:40:19 +0500

    Update settings.py

--- asikow/settings.py
+++ asikow/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
 from pathlib import Path
-import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
